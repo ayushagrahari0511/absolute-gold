287a37b845e35c960b2f320a617ed6984503e2ad
ayushagrahari0511 <ayushagrahari0511@protonmail>
2022-06-22 17:37:30 +0530

    initial commit

---
+++ components/Home/video/Video.js
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from 'react'
+import styles from './Video.module.css'
+
+const Video = () => {
+  const [isPlay, setIsPlay] = useState(false)
+  const videoRef = useRef(null);
+  return (
+    <div className={styles.wrapper}>
+      <video autoPlay loop muted ref={videoRef}>
+        <source src="/B2BGold.webm" type="video/webm" />
+      </video>
+      <div className={styles.skip}>
+        SKIP
+        <i className='fas fa-hand-point-right' />
+      </div>
+      <div className={styles.play} >
+        {
+          isPlay ?
+            <i class="fa-solid fa-volume-xmark" onClick={() => {
+              setIsPlay(false)
+              videoRef.current.muted = true;
+            }} />
+            :
+            <i class="fa-solid fa-volume-high" onClick={() => {
+              setIsPlay(true)
+              videoRef.current.muted = false;
+            }} />
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Video
