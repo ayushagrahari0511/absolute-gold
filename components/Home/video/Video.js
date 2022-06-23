b229970c348a4d799b461b79e0bf0d50dcffd6d3
ayushagrahari0511 <ayushagrahari0511@protonmail>
2022-06-23 15:54:16 +0530

    second commit

--- components/Home/video/Video.js
+++ components/Home/video/Video.js
@@ -1,36 +1,39 @@
 import React, { useRef, useState } from 'react'
 import styles from './Video.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandPointRight, faVolumeHigh, faVolumeXmark } from '@fortawesome/free-solid-svg-icons'
 
 const Video = () => {
   const [isPlay, setIsPlay] = useState(false)
   const videoRef = useRef(null);
   return (
     <div className={styles.wrapper}>
-      <video 
-      autoPlay 
-      loop 
-      muted 
-      ref={videoRef} 
-      src="/B2BGold.mp4"
-      playsInline>
+      <video
+        autoPlay
+        loop
+        muted
+        ref={videoRef}
+        src="/B2BGold.mp4"
+        playsInline>
         <source src="/B2BGold.webm" type="video/webm" />
       </video>
       <div className={styles.skip}>
         SKIP
-        <i className='fas fa-hand-point-right' />
+        <FontAwesomeIcon icon={faHandPointRight} className={styles.skipIcon} />
       </div>
       <div className={styles.play} >
         {
           isPlay ?
-            <i className="fa-solid fa-volume-xmark" onClick={() => {
+            <FontAwesomeIcon className={styles.icon} icon={faVolumeXmark} onClick={() => {
               setIsPlay(false)
               videoRef.current.muted = true;
             }} />
             :
-            <i className="fa-solid fa-volume-high" onClick={() => {
+            <FontAwesomeIcon className={styles.icon} icon={faVolumeHigh} onClick={() => {
               setIsPlay(true)
               videoRef.current.muted = false;
-            }} />
+            }}
+            />
         }
       </div>
     </div>
